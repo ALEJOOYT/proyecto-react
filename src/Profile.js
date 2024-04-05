8eb5713a1f2bbd4b56781ea737aabf41c6af2274
ALEJOO <zallesrene@gmail.com>
2024-04-05 09:19:52 -0400

    Arreglando js

--- src/Profile.js
+++ src/Profile.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Profile.css'; // Asegúrate de tener un archivo CSS llamado Profile.css en la misma ubicación
+import './Profile.css';
 
 function Profile() {
   return (
@@ -8,7 +8,6 @@ function Profile() {
         <figure>
           <img src="./assets/images/avatar-jessica.jpeg" alt="" />
         </figure>
-        <button>botonJava</button>
         <div className="nombre-titulo">
           <h2>Jessica Randall</h2>
           <h4>Londres, Reino Unido</h4>
@@ -20,7 +19,7 @@ function Profile() {
           <li><a href="#">LinkedIn</a></li>
           <li><a href="#">Twitter</a></li>
           <li><a href="#">Instagram</a></li>
-          <li><h6>SeedProgramming</h6></li> {/* Corregido: h6 debe estar dentro de li */}
+          <li><h6>SeedProgramming</h6></li> {}
         </ul>
       </div>
     </div>

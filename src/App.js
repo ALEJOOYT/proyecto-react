57390acc19543f0c89c694266c57ac7786f8ffbd
ALEJOO <zallesrene@gmail.com>
2024-04-06 14:00:00 -0400

    restaurando

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import Profile from './Profile'; // Importa el componente Profile
+import Tarjeta from './Componentes/Tarjeta';
+import './App.css'
 
 function App() {
   return (
